be9a053fe8376cabae0ba973b1a251a6ee72d482
cat-coder <cat-coder>
2021-08-14 22:06:44 -0500

    some comment has added

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -1,3 +1,9 @@
+//The Node.js JWT middleware checks that the JWT token received in the http request from the client is valid before allowing access to the API, 
+//if the token is invalid a 401 Unauthorized response is returned.
+
+//The JWT middleware is configured to make all routes secure except for the authenticate route (/users/authenticate) which is publicly accessible.
+
+
 const expressJwt = require('express-jwt');
 const config = require('config.json');
 
